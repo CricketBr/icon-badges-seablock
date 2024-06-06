ef955f6856c644d06cde9c282df0b430b865c179
CricketBr <90658143+CricketBr@users.noreply.github.com>
2024-06-06 11:44:38 -0400

    Release 0.2.02
    Split into 2 mods:
    - Icon Badges Groups -- A library.
    - Icon Badges Groups SeaBlock -- Data and a few function calls

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,139 +1,28 @@
-if not cb then cb = {} end
-if not cb.sib then cb.sib = {} end
-
--- first commit in branch to make barrels
+if not cb then cb={} end
+if not cb.ibg then cb.ibg={} end
 
 
-require('import-table')
+local b
+b=0
 
-local bpoint = 1
+-- get the settings file, we know which settings are ours
+local lsettings = require('settings-table-file')
+local lbigtablecsv = require('bigtable-csv-file-seablock')
+-- convert the csv file to a lua structure
+local lbigtable = cb.ibg.csv_to_lua(lbigtablecsv)
 
-local sets_to_do = {}
-local ldebugflags = {}
+-- you can make the lua structure without making csv first,
+-- but I found the csv file much easier to work with,
+-- especially when there are 100s of items and/or fluids,
+-- and the badgegroups keep changing
 
-ldebugflags.log_entry = false
-ldebugflags.log_success = false
-ldebugflags.log_fail = true
+-- 
+-- for Nullius, add rows for boxes
 
-local function p(m, v)
-    log('ib_badge_log : ' .. m .. ' : name = ' .. v.name .. ', type = ' .. v.type .. ', badge = ' .. v.badge)
-end
-
-
-
-local function do_badge(v)
-    if ldebugflags.log_entry then p('entering', v) end
-    local ib_data = { ib_let_badge = v.badge, ib_let_corner = 'right-top' }
-    if pcall(Build_badge, data.raw[v.type][v.name], ib_data) then
-        -- if ldebugflags.log_success then p('success', v) end
-    else
-        if ldebugflags.log_fail then p('fail', v) end
-    end
-end
-
-
-local groups = -- This is duplicating info in settings file.
-{
-    'cb-sib-setting-aliens',
-    'cb-sib-setting-angelsores',
-    'cb-sib-setting-artifacts',
-    'cb-sib-setting-bioprocessing',
-    'cb-sib-setting-combat',
-    'cb-sib-setting-gems',
-    'cb-sib-setting-intermediates',
-    'cb-sib-setting-logchests',
-    'cb-sib-setting-logistics',
-    'cb-sib-setting-metals',
-    'cb-sib-setting-modules',
-    'cb-sib-setting-petrochem',
-    'cb-sib-setting-science',
-    'cb-sib-setting-stone',
-    'cb-sib-setting-vegetables',
-    'cb-sib-setting-watertreatment'
-}
-
-local sets_to_do = {} -- alien-badges-1
-
-for groupk, groupv in ipairs(groups) do
-    sets_to_do[settings.startup[groupv].value] = true
-end
-
-
-for _, v in ipairs(cb.sib.table) do
-    if sets_to_do[v.badgeset] then
-        local ib_data = { ib_let_badge = v.badge, ib_let_corner = 'right-top' }
-        if pcall(Build_badge, data.raw[v.type][v.name], ib_data) then
-            p('success', v)
-        else
-            p('fail', v)
-        end
-
-        if v.type == 'fluid' then -- do barrel
-            local bname = v.name .. '-barrel'
-            if data.raw.item[bname] then
-                if pcall(Build_badge, data.raw['item'][bname], ib_data) then
-                    if ldebugflags.log_success then p('success barrel', v) end
-                else
-                    if ldebugflags.log_fail then p('fail barrel', v) end
-                end
-            end
-        end
-    end
-end
+cb.ibg.do_badges(lsettings, lbigtable)
+-- This will do barrels automatically
+-- Don't worry about fluids that don't have barrels. do_badges checks for them.
 
 
 
 
---             local a, message
---             -- a, message = pcall(Build_badge, data.raw[v.type][v.name], ib_data)
---             -- log ('sbib '.. a .. message)
-
-
-
-
-local bp = 1 -- just a breakpoint
-
---         if v.type == 'fluid' then
---             local bname = v.name .. '-barrel'
---             data.raw.fluid[v.name].ib_let_badge = v.badge
---             -- data.raw.fluid[v.name].ib_let_corner = 'right-top'
-
---             if data.raw.item[bname] then
---                 data.raw.item[bname].ib_let_badge = v.badge
---                 -- data.raw.item[bname].ib_let_corner = 'right-top'
---             end
---         end
---     end
--- end
-
-
-
-
-
-
-
-
-
-
-
-
---     local t = false
-
-
---     local b = v.badgeset
---     if b == 'latinmetals' or b == 'englishmetals' then
---         b = 'metals'
---     end
---     -- do sciencenames same way as metals, but currently sciences names aren't bringing up the
-
-
-
---     if v.badgeset == 'englishmetals' and bsetting == 'English' then t = true end
---     if v.badgeset == 'latinmetals' and bsetting == 'Latin' then t = true end
-
---     if t then
-
---     end
--- end
-
--- local breakpt = 1
---
+++ settings-table-file.lua
@@ -0,0 +1,21 @@
+-- in seablock
+
+return{
+    ['cb-ibg-sb-alien'] = { 'aliens-group-1' },
+    ['cb-ibg-sb-angelsores'] = { 'angelsores-group-names','angelsores-group-colors','angelsores-group-numbers' },
+    ['cb-ibg-sb-artifacts'] = { 'artifacts-group-colors', 'artifacts-group-numbers' },
+    ['cb-ibg-sb-bioprocessing'] = {'bioprocessing-group-1'},
+    ['cb-ibg-sb-combat'] = {'combat-group-1'},
+    ['cb-ibg-sb-gems'] = {'gems-group-1','gems-group-colors'},
+    ['cb-ibg-sb-intermediates'] = {'intermediates-group-1'},
+    ['cb-ibg-sb-logchests'] = {'logchests-group-1'},
+    ['cb-ibg-sb-logistics'] = {'logistics-group-1'},
+    ['cb-ibg-sb-metals'] = {'metals-group-scientific', 'metals-group-english'},
+    ['cb-ibg-sb-modules'] = {'modules-group-1'},
+    ['cb-ibg-sb-petrochem'] = {'petrochem-group-1'},
+    ['cb-ibg-sb-science'] = {'science-group-1','science-group-colors'},
+    ['cb-ibg-sb-stone'] = {'stone-group-1'},
+    ['cb-ibg-sb-vegetables'] = {'vegetables-group-1'},
+    ['cb-ibg-sb-vehicles'] = {'vehicles-group-1'},
+    ['cb-ibg-sb-watertreatment'] = {'watertreatment-group-1'},
+}
